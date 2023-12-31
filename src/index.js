ca3610c95172c4a6deb46e19d75a3d5f0edf5632
Khuong Tran <dbcgdhh@gmail.com>
2023-12-31 09:00:50 +0700

    Init form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("initialized");
+let words = [];
+
+document.addEventListener("DOMContentLoaded", event => {
+  fetch("words.txt")
+    .then(res => res.text())
+    .then(text => {
+      words = text.split("\n");
+    })
+    .catch(e => console.error(e));
+});
+
+function findWords(e) {
+  e.preventDefault();
+  const searchTerm = e.target.elements.searchTerm.value;
+  if (words.includes(searchTerm)) console.log(true);
+  else console.log(false);
+}
